2ab1002b1af78ad362384943c9e2637d1722f91d
unknown <akashpawar6340@mail.com>
2025-12-14 18:49:01 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    
+    output: 'export',
     images: {
-        domains: ['images.unsplash.com'],
-    }
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
